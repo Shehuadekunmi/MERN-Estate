88977fe38ca4c4ebd804951959003dce34df8bfb
Shehuadekunmi <sirkumyemo@gmail.com>
2023-12-21 11:42:07 +0000

    done with pages and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Header from './components/Header'
+import Signin from './pages/Signin'
+import Singup from './pages/Singup'
+import PrivateRoute from './components/PrivateRoute'
+import CreateListing from './pages/CreateListing'
+import UpdateListing from './pages/UpdateListing'
+import Listing from './pages/Listing'
+import Search from './pages/Search'
+
 
 function App() {
- 
 
   return (
     <>
-     
-      <h1 className='text-red-500'>App</h1>
+    
+     <Router>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/search' element={<Search/>}/>
+        <Route path='/signup' element={<Singup/>}/>
+        <Route path='/signin' element={<Signin/>}/>
+        <Route path='/listing/:listingId' element={<Listing/>}/>
+        <Route  element={<PrivateRoute/>}>
+          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/createlisting' element={<CreateListing/>}/>          
+          <Route path='/updatelisting/:listingId' element={<UpdateListing/>}/>          
+        </Route>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+     </Router>
+      
     </>
   )
 }
