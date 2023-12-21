6247da2f915c37080921ae1cf11cf6278fd3ed04
Shehuadekunmi <sirkumyemo@gmail.com>
2023-12-21 13:35:07 +0000

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
   return (
     <>
-    
+    <h1>hello</h1>
      <Router>
       <Header/>
       <Routes>
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': { 
+        target: 'http://localhost:3000', 
+        // https://mern-estate-server-zio1.onrender.com
+        secure: false
+      },
+    },
+  },
   plugins: [react()],
 })
